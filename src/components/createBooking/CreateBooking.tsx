8e57303e2c09500e5c955f1493a0ce3a88ae286f
Gurparshad Sharma <gurparshad.sharma@visioncraft.ee>
2024-02-04 18:26:22 +0200

    style changes

--- src/components/createBooking/CreateBooking.tsx
+++ src/components/createBooking/CreateBooking.tsx
@@ -2,8 +2,12 @@ import {useNavigate} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {RootState} from "../../app/store";
 import {resetForm} from "../../features/form/formSlice";
+import {calculatePerNightPrice} from "../../utils/calculatePerNightPrice";
+import {formatDate} from "../../utils/formatDate";
 import {SelectedProduct} from "../../utils/types";
 import Button from "../button/Button";
+import "./createBooking.scss";
+
 interface CreateBookingProps {
   onBack: () => void;
 }
@@ -11,32 +15,42 @@ interface CreateBookingProps {
 const CreateBooking: React.FC<CreateBookingProps> = ({onBack}) => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
+  const formData = useAppSelector((state: RootState) => state.form.form.formData);
+  const {startDate, endDate, room, products} = formData;
+
+  const calculateGrandTotal = () => {
+    const roomPrice = formData.room?.discountedPrice ?? formData.room?.totalPrice;
+    const totalProductPrices = products.reduce((accumulator, product) => {
+      return accumulator + product.totalPrice;
+    }, 0);
+    // @ts-ignore
+    return roomPrice + totalProductPrices;
+  };
+
   const handleSubmit = () => {
-    // persistor.purge(["form"]);
     dispatch(resetForm());
     navigate("/success");
   };
-  const formData = useAppSelector((state: RootState) => state.form.form.formData);
-  const {startDate, endDate, room, products} = formData;
-  console.log("room-->>", room);
+
   return (
     <div>
-      <h2>Create Booking</h2>
-      <p>Please check the details</p>
+      <h2>Booking Details</h2>
+      <p>Please check your booking details below</p>
       <div>
         {/* @ts-ignore */}
-        <p>Start Date: {startDate.toString()}</p>
+        <p>Check In: {formatDate(startDate)}</p>
       </div>
       <div>
         {/* @ts-ignore */}
-        <p>End Date: {endDate.toString()}</p>
+        <p>Check Out: {formatDate(endDate)}</p>
       </div>
       <div style={{border: "1px solid red"}}>
         <p>Room details</p>
         <div>
-          <img src={room?.image} alt="room-image" />
           <p>Name:{room?.name}</p>
-          <p>Price per night:{room?.pricePerNight}</p>
+          {/* @ts-ignore */}
+          <p>Price per night:{calculatePerNightPrice(room?.pricePerNight, room?.priceTaxPercentage)}</p>
           <p>Number of nights: {room?.numberOfNights}</p>
           <p>Total Price: {room?.totalPrice}</p>
           {room?.discountedPrice !== 0 && <p>Discounted Price{room?.discountedPrice}</p>}
@@ -45,11 +59,18 @@ const CreateBooking: React.FC<CreateBookingProps> = ({onBack}) => {
       <div>
         <p>Products</p>
         {products.map((item: SelectedProduct) => (
-          <div style={{border: "1px solid green"}}>
-            <p>{item.name}</p>
-            <p>{item.totalPrice}</p>
+          <div>
+            <p>Name: {item.name}</p>
+            <p>Price per Night: {calculatePerNightPrice(item.priceNet, item.priceTaxPercentage)}</p>
+            <p>Total Price:{item.totalPrice}</p>
           </div>
         ))}
+      </div>
+
+      <div>
+        <p>
+          Grand Total: <span>{calculateGrandTotal()}</span>
+        </p>
       </div>
       <Button onClick={onBack}>Back</Button>
       <Button onClick={handleSubmit}>Create Booking</Button>
---
+++ src/utils/formatDate.ts
@@ -0,0 +1,14 @@
+export const formatDate = (date: Date): string => {
+  const newDate = new Date(date);
+
+  const year = newDate.getFullYear();
+  const month = newDate.toLocaleString('default', { month: 'short' });
+  const day = newDate.getDate();
+
+  const hours = newDate.getHours().toString().padStart(2, '0');
+  const minutes = newDate.getMinutes().toString().padStart(2, '0');
+
+  const formattedStartDate = `${day} ${month} ${year} ${hours}:${minutes}`;
+
+  return formattedStartDate;
+}
