c5570cd2cf9908adf25fa225cfb6505d04e0173f
Gurparshad Sharma <gurparshad.sharma@visioncraft.ee>
2024-02-06 01:40:33 +0200

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Success from "./pages/success/Success";
 import {ReactElement} from "react";
 import BookRoom from "./pages/bookRoom/BookRoom";
+import Navbar from "./components/navbar/Navbar";
 
 interface ProtectedRouteProps {
   element: ReactElement;
@@ -17,12 +18,15 @@ const App = () => {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/book-room" element={<BookRoom />} />
-        <Route path="/success" element={<ProtectedRoute element={<Success />} />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book-room" element={<BookRoom />} />
+          <Route path="/success" element={<ProtectedRoute element={<Success />} />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import {Link} from "react-router-dom";
+import styles from "./navbar.module.scss";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link to="/" className={styles.navLink}>
+            Home
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/book-room" className={styles.navLink}>
+            Book Room
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
